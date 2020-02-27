3e705a4a33aa01a3cb692b6c91476e21bf907e6e
Michael Paulukonis <mpauluko@travelers.com>
2020-02-26 22:46:56 -0500

    Adding actual gif editor Was not part of original repo - downloaded from web and edited out the ads (!!!) No other changes yet - need to integrate tools, etc. TODO: would like to englarge images, also step through frames. Wheee!!!

---
+++ animated.gif.maker_files/13893.js
@@ -0,0 +1,48 @@
+BAP.copyJSON({
+  "data": {
+    "icon_position": "top-right",
+    "default_icon": "_us",
+    "mobile_in_app_url": "",
+    "default_footer": "Privacy Controls by Evidon, Inc.",
+    "icon_display": "expandable",
+    "icon_grayscale": 30,
+    "container_opacity": 70,
+    "offset_x": 0,
+    "offset_y": 0,
+    "generic_icon": false,
+    "icon_delay": 0,
+    "nid": 13893,
+    "nwid": null,
+    "aid": 2446,
+    "icid": null,
+    "skip_L2": false,
+    "behavioral": "custom",
+    "generic_text": "This ad was delivered on behalf of Allstate. It was automatically matched to your computer's browsing activity. Allstate is committed to providing you with transparency and control over the type of advertising you see by us.\r\n",
+    "adv_name": "Allstate",
+    "adv_msg": "Allstate Privacy Policy",
+    "adv_logo": "http://c.betrad.com/a/l/n/2446/13893/us/f08f4ea8-28a4-4b96-b87b-7eb472971b8b.png",
+    "adv_link": "http://www.allstate.com/about/privacy-statement-aic.aspx",
+    "mobile_message": "Tap to edit advertising preferences",
+    "display_mobile_overlay": false,
+    "mobile_advertiser_logo_url": "",
+    "default_icon_text": "AdChoices",
+    "default_generic1": "This ad has been matched to your interests. It was selected for you based on your browsing activity.",
+    "default_generic2": "This ad may have been matched to your interests based on your browsing activity.",
+    "default_generic3": "helped",
+    "default_generic4": "determine that you might be interested in an ad like this.",
+    "default_generic5": "select this ad for you.",
+    "default_generic6": "selected this ad for you.",
+    "default_link1": "More information & opt-out options",
+    "default_link2": "What is interest based advertising",
+    "default_link3": "Learn about your choices",
+    "ecid": null,
+    "hide_wi": false,
+    "hide_cl": false,
+    "server": [
+      {
+        "name": "DoubleClick"
+      }
+    ],
+    "message_properties": {}
+  }
+});
---
+++ animated.gif.maker_files/blueprint.js
@@ -0,0 +1,284 @@
+window.blueprint = {
+	"width": 728,
+	"height": 90,
+	"images": [
+		"logo_vimeo.png",
+		"txt_1.png",
+		"txt_2.png",
+		"txt_3.png",
+		"cta_1.png",
+		"ctaHover_1.png",
+		"logo_reelboy.png",
+		"hand.png"
+	],
+	"elements": {
+		"stage": {
+			"parent": "banner",
+			"id": "stage",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 728,
+			"height": 90
+		},
+		"bg": {
+			"parent": "banner",
+			"id": "bg",
+			"retina": false,
+			"left": 0,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 728,
+			"height": 90
+		},
+		"logo_vimeo": {
+			"parent": "banner",
+			"id": "logo_vimeo",
+			"retina": true,
+			"left": 22,
+			"top": 34,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "logo_vimeo.png",
+			"width": 82,
+			"height": 23
+		},
+		"txt_1": {
+			"parent": "banner",
+			"id": "txt_1",
+			"retina": true,
+			"left": 136,
+			"top": -4.625,
+			"safeArea": 25,
+			"lineHeight": 27,
+			"backgroundImage": "txt_1.png",
+			"width": 360,
+			"height": 98
+		},
+		"txt_2": {
+			"parent": "banner",
+			"id": "txt_2",
+			"retina": true,
+			"left": 136,
+			"top": -3.25,
+			"safeArea": 25,
+			"lineHeight": 26.75,
+			"backgroundImage": "txt_2.png",
+			"width": 299,
+			"height": 71
+		},
+		"txt_3": {
+			"parent": "banner",
+			"id": "txt_3",
+			"retina": true,
+			"left": 146,
+			"top": 37,
+			"safeArea": 15,
+			"lineHeight": 15.600000000000001,
+			"backgroundImage": "txt_3.png",
+			"width": 147,
+			"height": 43
+		},
+		"cta": {
+			"parent": "banner",
+			"id": "cta",
+			"retina": true,
+			"left": 582,
+			"top": 27,
+			"safeArea": 0,
+			"lineHeight": 15.600000000000001
+		},
+		"cta_1": {
+			"parent": "cta",
+			"id": "cta_1",
+			"retina": true,
+			"backgroundImage": "cta_1.png"
+		},
+		"ctaHover": {
+			"parent": "banner",
+			"id": "ctaHover",
+			"retina": true,
+			"left": 582,
+			"top": 27,
+			"safeArea": 0,
+			"lineHeight": 15.600000000000001
+		},
+		"ctaHover_1": {
+			"parent": "ctaHover",
+			"id": "ctaHover_1",
+			"retina": true,
+			"backgroundImage": "ctaHover_1.png"
+		},
+		"logo_vimeo_pos1": {
+			"parent": "banner",
+			"id": "logo_vimeo_pos1",
+			"retina": false,
+			"left": 22,
+			"top": 34,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 82,
+			"height": 23
+		},
+		"logo_vimeo_pos2": {
+			"parent": "banner",
+			"id": "logo_vimeo_pos2",
+			"retina": false,
+			"left": -108,
+			"top": 34,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 82,
+			"height": 23
+		},
+		"logo_reelboy": {
+			"parent": "banner",
+			"id": "logo_reelboy",
+			"retina": true,
+			"left": 29,
+			"top": 22,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "logo_reelboy.png",
+			"width": 54,
+			"height": 46
+		},
+		"logo_reelboy_pos2": {
+			"parent": "banner",
+			"id": "logo_reelboy_pos2",
+			"retina": false,
+			"left": 29,
+			"top": 22,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 54,
+			"height": 46
+		},
+		"logo_reelboy_pos3": {
+			"parent": "banner",
+			"id": "logo_reelboy_pos3",
+			"retina": false,
+			"left": -11,
+			"top": 102,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 54,
+			"height": 46
+		},
+		"logo_reelboy_pos1": {
+			"parent": "banner",
+			"id": "logo_reelboy_pos1",
+			"retina": false,
+			"left": -11,
+			"top": 102,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 54,
+			"height": 46
+		},
+		"hand_pos2": {
+			"parent": "banner",
+			"id": "hand_pos2",
+			"retina": false,
+			"left": 89,
+			"top": 16,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 43,
+			"height": 53
+		},
+		"hand_pos1": {
+			"parent": "banner",
+			"id": "hand_pos1",
+			"retina": false,
+			"left": 138,
+			"top": 112,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 44,
+			"height": 53
+		},
+		"hand": {
+			"parent": "banner",
+			"id": "hand",
+			"retina": true,
+			"left": 403,
+			"top": 36,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"backgroundImage": "hand.png",
+			"width": 43,
+			"height": 52
+		},
+		"top": {
+			"parent": "banner",
+			"id": "top",
+			"retina": false,
+			"left": 0,
+			"top": -10,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 728,
+			"height": 10
+		},
+		"bottom": {
+			"parent": "banner",
+			"id": "bottom",
+			"retina": false,
+			"left": 0,
+			"top": 90,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 728,
+			"height": 10
+		},
+		"left": {
+			"parent": "banner",
+			"id": "left",
+			"retina": false,
+			"left": -10,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 90
+		},
+		"right": {
+			"parent": "banner",
+			"id": "right",
+			"retina": false,
+			"left": 728,
+			"top": 0,
+			"safeArea": 0,
+			"lineHeight": 0,
+			"width": 10,
+			"height": 90
+		}
+	},
+	"settings": {
+		"filename": "Q3-2019_EN_mid-funnel_marketer_custom-logo_2_learn-more_728x90_HTML5",
+		"market": "EN",
+		"campaign": "Q3-2019",
+		"funnel": "mid-funnel",
+		"segment": "marketer",
+		"feature": "custom-logo",
+		"copyVariation": "2",
+		"size": "728x90",
+		"marketIndex": "1",
+		"id": "marketercustom-logo2",
+		"copy": {
+			"copy1": "Our player is all up in your business.",
+			"copy2": "Customizable Player.",
+			"copy3": "A Vimeo Feature",
+			"copy4": "Learn More",
+			"copy5": ""
+		},
+		"fileType": "jpg",
+		"fallback": true,
+		"HtmlfileType": "html",
+		"backgroundColor": "#e59fb9"
+	}
+};
